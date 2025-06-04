79b9240b390e0e0aeaa447f8c5c5c5bcc0075de4
Mitch <mitch.hare03@gmail.com>
2025-06-04 20:22:47 +1000

    Implement progress bar animation

--- imports/ui/components/player/Controls.jsx
+++ imports/ui/components/player/Controls.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
   BsFillFastForwardFill,
   BsFillPauseFill,
@@ -12,19 +12,53 @@ import {
 import { useAudioPlayerContext } from '../../../contexts/AudioPlayerContext';
 
 export const Controls = () => {
-  const { currentTrack, audioRef, setDuration, progressBarRef } =
+  const { currentTrack, audioRef, setDuration, duration, setTimeProgress, progressBarRef } =
     useAudioPlayerContext();
   const [isShuffle, setIsShuffle] = useState(false);
   const [isRepeat, setIsRepeat] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
+  const playAnimationRef = useRef(null);
+
+  const updateProgress = useCallback(() =>{
+    if (audioRef.current && progressBarRef.current && duration) {
+      const currentTime= audioRef.current.currentTime;
+      setTimeProgress(currentTime);
+      progressBarRef.current.value = currentTime.toString();
+      progressBarRef.current.style.setProperty(
+        '--range-progress',
+        `${(currentTime / duration) * 100}%`
+      );
+    }
+  }, [duration,setTimeProgress, audioRef, progressBarRef]);
+
+  const startAnimation = useCallback(() =>{
+    if (audioRef.current && progressBarRef.current && duration) {
+      const animate = () => {
+        updateProgress();
+        playAnimationRef.current = requestAnimationFrame(animate);
+      };
+      playAnimationRef.current = requestAnimationFrame(animate);
+    }
+  }, [updateProgress, duration, audioRef, progressBarRef]);
 
   useEffect(() => {
     if (isPlaying) {
       audioRef.current?.play();
+      startAnimation();
     } else {
       audioRef.current?.pause();
+      if (playAnimationRef.current) {
+        cancelAnimationFrame(playAnimationRef.current);
+        playAnimationRef.current = null;
+      }
+      updateProgress(); // Ensure progress is updated immediately when paused
     }
-  }, [isPlaying, audioRef]);
+    return () => {
+      if (playAnimationRef.current !== null ) {
+        cancelAnimationFrame(playAnimationRef.current);
+      }
+    };
+  }, [isPlaying, startAnimation, updateProgress, audioRef]);
 
   const onLoadedMetadata = () => {
     console.log('abc');
