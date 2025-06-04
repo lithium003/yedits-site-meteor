b9b17d2c5f5d43ba81134af11c10f44f16b4ccd4
Mitch <mitch.hare03@gmail.com>
2025-06-04 16:53:33 +1000

    Add Progress Bar with custom styling in customize-progress-bar.css

--- client/main.jsx
+++ client/main.jsx
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
 import '/imports/Router'; // Router (Client-Side)
+import './styles/customize-progress-bar.css';
 
 Meteor.startup(() => {});
--- imports/ui/components/player/ProgressBar.jsx
+++ imports/ui/components/player/ProgressBar.jsx
@@ -1,1 +1,13 @@
-export const ProgressBar = () => {};
+import React from 'react';
+
+export const ProgressBar = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center gap-5 w-full">
+        <span>00:00</span>
+        <input className="max-w-[80%] bg-gray-300" type="range" />
+        <span>03:34</span>
+      </div>
+    </>
+  );
+};
