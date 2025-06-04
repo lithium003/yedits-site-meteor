6177743025d015cbf4ebdc8f3cd23648e2eecfa1
Mitch <mitch.hare03@gmail.com>
2025-06-04 15:08:03 +1000

    Add custom controls (non-functional currently) with Controls.jsx

--- imports/ui/components/player/Controls.jsx
+++ imports/ui/components/player/Controls.jsx
@@ -1,11 +1,52 @@
+import React, { useState } from 'react';
+import {
+  BsFillFastForwardFill,
+  BsFillPauseFill,
+  BsFillPlayFill,
+  BsFillRewindFill,
+  BsSkipEndFill,
+  BsSkipStartFill,
+  BsShuffle,
+  BsRepeat
+} from 'react-icons/bs';
 import { useAudioPlayerContext } from '../../../contexts/AudioPlayerContext';
-import React from 'react';
 
 export const Controls = () => {
   const { currentTrack } = useAudioPlayerContext();
+  const [isShuffle, setIsShuffle] = useState(false);
+  const [isRepeat, setIsRepeat] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+
   return (
     <>
-      <audio src={currentTrack.src} controls />
+      <div className="flex gap-4 items-center">
+        <audio src={currentTrack.src} />
+        <button onClick={() => {}}>
+          <BsSkipStartFill size={20} />
+        </button>
+        <button onClick={() => {}}>
+          <BsFillRewindFill size={20} />
+        </button>
+        <button onClick={() => setIsPlaying(prev => !prev)}>
+          {isPlaying ? (
+            <BsFillPauseFill size={30} />
+          ) : (
+            <BsFillPlayFill size={30} />
+          )}
+        </button>
+        <button onClick={() => {}}>
+          <BsFillFastForwardFill size={20} />
+        </button>
+        <button onClick={() => {}}>
+          <BsSkipEndFill size={20} />
+        </button>
+        <button onClick={() => setIsShuffle(prev => !prev)}>
+          <BsShuffle size={20} className={isShuffle ? 'text-[#f50]' : ''} />
+        </button>
+        <button onClick={() => setIsRepeat(prev => !prev)}>
+          <BsRepeat size={20} className={isRepeat ? 'text-[#f50]' : ''} />
+        </button>
+      </div>
     </>
   );
 };
