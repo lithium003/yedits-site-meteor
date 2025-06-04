b5bd04256c00f13b14897a770aef3db18ff0ddac
Mitch <mitch.hare03@gmail.com>
2025-06-04 22:50:41 +1000

    Tie volume to volume controls, fix showing as mute under 5% volume

--- imports/ui/components/player/VolumeControl.jsx
+++ imports/ui/components/player/VolumeControl.jsx
@@ -1,5 +1,6 @@
-import React, { ChangeEvent, useState } from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 import { IoMdVolumeHigh, IoMdVolumeLow, IoMdVolumeOff } from 'react-icons/io';
+import { useAudioPlayerContext } from '../../../contexts/AudioPlayerContext';
 
 export const VolumeControl = () => {
   const [volume, setVolume] = useState(60);
@@ -8,11 +9,19 @@ export const VolumeControl = () => {
     setVolume(Number(e.target.value));
   };
 
+  const { audioRef } = useAudioPlayerContext();
+  useEffect(() => {
+    if (audioRef.current) {
+      audioRef.current.volume = volume / 100;
+      audioRef.current.muted = muteVolume;
+    }
+  }, [volume, audioRef, muteVolume]);
+
   return (
     <>
       <div className="flex items-center gap-3">
         <button onClick={() => setMuteVolume(prev => !prev)}>
-          {muteVolume || volume < 5 ? (
+          {muteVolume || volume === 0 ? (
             <IoMdVolumeOff size={25} />
           ) : volume < 40 ? (
             <IoMdVolumeLow size={25} />
