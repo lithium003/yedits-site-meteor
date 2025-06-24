1d51ca84bf22d6bc6d37099c60265ca931bdc057
Mitch <mitch.hare03@gmail.com>
2025-06-24 17:49:57 +1000

    Set CompShelf width to only span across 5 200x200 CompItems

--- imports/ui/components/CompShelf.jsx
+++ imports/ui/components/CompShelf.jsx
@@ -2,11 +2,15 @@ import React, { forwardRef } from 'react';
 import { CompItem } from './CompItem';
 
 export const CompShelf = forwardRef(({ items }, ref) => {
+  // Calculate width: 200px (CompItem width) * 5 + 8px (gap) * 4 + 16px (padding) + 8px (scrollbar) + 16px (idk) + 8px (right padding)
+  const shelfWidth = 200 * 5 + 8 * 4 + 16 + 8 + 16 + 8;
+
   return (
     <>
       <div
         ref={ref}
-        className="flex flex-nowrap bg-black/50 py-3 px-1 rounded-xl overflow-auto
+        style={{ width: `${shelfWidth}px` }}
+        className="flex flex-nowrap overflow-x-auto bg-black/50 py-3 px-2 rounded-xl
         [&::-webkit-scrollbar]:w-2
         [&::-webkit-scrollbar-track]:rounded-full
         [&::-webkit-scrollbar-track]:bg-black
@@ -16,7 +20,10 @@ export const CompShelf = forwardRef(({ items }, ref) => {
         "
       >
         {items.map(item => (
-          <div className="shelf-item px-1" key={item.id}>
+          <div
+            className="shelf-item flex-shrink-0 first:ml-0 ml-2 mr-2 last:mr-0"
+            key={item.id}
+          >
             <CompItem comp={item} />
           </div>
         ))}
