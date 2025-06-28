a8dbeff50e1b078496821b05d62df8ed69838ef5
Mitch <mitch.hare03@gmail.com>
2025-06-28 15:04:11 +1000

    Refactor getSearchResults to switch queries based on collection type, and conditionally adding era filtering

--- imports/api/methods/Search.js
+++ imports/api/methods/Search.js
@@ -1,35 +1,60 @@
 import { Meteor } from 'meteor/meteor';
-import { COMPS, YEDITORS } from '/imports/api/collections/AvailableCollections';
+import {
+  COMPS,
+  EDITS,
+  YEDITORS
+} from '/imports/api/collections/AvailableCollections';
 import { convertPath, db } from '/server/Firestore';
 
 Meteor.methods({
   async getSearchResults({
     collection = COMPS,
     searchTerm = '',
+    era = null,
     lastId = null
   }) {
     const numResults = 5;
     console.log(collection);
     try {
-      let query = await db
-        .collection(collection)
-        .where('name_search', '>=', searchTerm)
-        .where('name_search', '<=', searchTerm + '\uf8ff')
-        .orderBy('rating', 'desc')
-        .orderBy('name_search')
-        .orderBy('__name__')
-        .limit(numResults);
-
-      // TODO: Unify the name fields of the different collections to make this block unnecessary
-      if (collection === YEDITORS) {
-        query = await db
-          .collection(collection)
-          .where('display_name_search', '>=', searchTerm)
-          .where('display_name_search', '<=', searchTerm + '\uf8ff')
-          .orderBy('display_name_search')
-          .orderBy('__name__')
-          .limit(numResults);
+      // Ordering by name_search before rating would be more performance- and cost-
+      // efficient, but this would mean when you search for "vultures"
+      let query = await db.collection(collection);
+      /* eslint-disable indent */
+      switch (collection) {
+        case COMPS:
+          era && (query = query.where('era', '==', era));
+          query = query
+            .where('standalone_edit', '==', false)
+            .where('name_search', '>=', searchTerm)
+            .where('name_search', '<=', searchTerm + '\uf8ff')
+            .orderBy('rating', 'desc')
+            .orderBy('name_search')
+            .orderBy('__name__')
+            .limit(numResults);
+          break;
+        case EDITS:
+          era && (query = query.where('era', '==', era));
+          query = query
+            .where('name_search', '>=', searchTerm)
+            .where('name_search', '<=', searchTerm + '\uf8ff')
+            .orderBy('rating', 'desc')
+            .orderBy('name_search')
+            .orderBy('__name__')
+            .limit(numResults);
+          break;
+        case YEDITORS:
+          query = query
+            .where('display_name_search', '>=', searchTerm)
+            .where('display_name_search', '<=', searchTerm + '\uf8ff')
+            .orderBy('display_name_search')
+            .orderBy('__name__')
+            .limit(numResults);
+          break;
+        default:
+          console.error(`Error fetching results for ${collection}`);
+        // throw new Meteor.Error('firebase-error', 'Failed to fetch results');
       }
+      /* eslint-enable indent */
 
       // We need the actual document ID, but we modify its elements when we return them as a list.
       // TODO: Find a way to keep the doc so we don't have to get() it again
@@ -56,10 +81,6 @@ Meteor.methods({
         };
       });
 
-      // console.log(results); // Or return to client via a Meteor method
-      if (collection === YEDITORS) {
-        console.log(results);
-      }
       return results;
     } catch (error) {
       console.error(`Error fetching results for ${collection}: ${error}`);
