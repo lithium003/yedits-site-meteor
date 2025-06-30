71521ecbadb368e222d07e2813c951929b4ee03d
Mitch <mitch.hare03@gmail.com>
2025-06-30 22:29:59 +1000

    Mellow out era colours a little

--- imports/utils/eras.js
+++ imports/utils/eras.js
@@ -3,315 +3,315 @@ export const ERAS = [
     id: 'beforeTcd',
     name: 'Before TCD',
     style: {
-      color: 'rgb(246, 178, 107)',
-      backgroundColor: 'rgb(166, 28, 0)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(240, 185, 120)',
+      backgroundColor: 'rgb(145, 45, 15)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'tcd',
     name: 'TCD',
     style: {
-      color: 'rgb(239, 162, 1)',
-      backgroundColor: 'rgb(87, 19, 20)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(230, 170, 25)',
+      backgroundColor: 'rgb(95, 35, 35)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'lr',
     name: 'LR',
     style: {
-      color: 'rgb(184, 146, 21)',
-      backgroundColor: 'rgb(41, 10, 7)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(175, 155, 45)',
+      backgroundColor: 'rgb(55, 25, 20)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'graduation',
     name: 'Graduation',
     style: {
-      color: 'rgb(234, 80, 171)',
-      backgroundColor: 'rgb(65, 34, 87)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(225, 100, 180)',
+      backgroundColor: 'rgb(75, 45, 95)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: '808s',
     name: '808s',
     style: {
-      color: 'rgb(242, 0, 1)',
-      backgroundColor: 'rgb(195, 205, 204)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(186,17,17)',
+      backgroundColor: 'rgb(185, 195, 194)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'gaj',
     name: 'GAJ',
     style: {
-      color: 'rgb(255, 255, 0)',
-      backgroundColor: 'rgb(108, 5, 170)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(245, 245, 30)',
+      backgroundColor: 'rgb(118, 25, 155)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'mbdtf',
     name: 'MBDTF',
     style: {
-      color: 'rgb(247, 213, 80)',
-      backgroundColor: 'rgb(177, 29, 51)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(240, 205, 95)',
+      backgroundColor: 'rgb(160, 45, 65)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'wtt',
     name: 'WTT',
     style: {
-      color: 'rgb(255, 197, 17)',
-      backgroundColor: 'rgb(132, 108, 71)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(245, 185, 35)',
+      backgroundColor: 'rgb(120, 100, 80)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'cruelSummer',
     name: 'Cruel Summer',
     style: {
-      color: 'rgb(241, 241, 241)',
-      backgroundColor: 'rgb(133, 133, 133)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(230, 230, 230)',
+      backgroundColor: 'rgb(120, 120, 120)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'tgfd',
     name: 'TGFD',
     style: {
-      color: 'rgb(61, 133, 198)',
-      backgroundColor: 'rgb(239, 239, 239)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(75, 125, 185)',
+      backgroundColor: 'rgb(230, 230, 230)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'yeezus',
     name: 'Yeezus',
     style: {
-      color: 'rgb(255, 0, 0)',
-      backgroundColor: 'rgb(243, 243, 243)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(235, 25, 25)',
+      backgroundColor: 'rgb(235, 235, 235)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'yeezus2',
     name: 'Yeezus 2',
     style: {
-      color: 'rgb(252, 29, 0)',
-      backgroundColor: 'rgb(50, 46, 40)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(235, 45, 15)',
+      backgroundColor: 'rgb(65, 60, 55)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'shmg',
     name: 'SHMG',
     style: {
-      color: 'rgb(212, 205, 195)',
-      backgroundColor: 'rgb(107, 95, 90)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(200, 195, 185)',
+      backgroundColor: 'rgb(95, 85, 80)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'swish',
     name: 'SWISH',
     style: {
-      color: 'rgb(49, 13, 23)',
-      backgroundColor: 'rgb(255, 122, 116)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(57,26,35)',
+      backgroundColor: 'rgb(240, 135, 130)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'tlop',
     name: 'TLOP',
     style: {
-      color: 'rgb(0, 0, 0)',
-      backgroundColor: 'rgb(245, 139, 87)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(25, 25, 25)',
+      backgroundColor: 'rgb(235, 150, 100)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'cruelWinter',
     name: 'Cruel Winter',
     style: {
-      color: 'rgb(102, 102, 102)',
-      backgroundColor: 'rgb(251, 243, 232)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(90, 90, 90)',
+      backgroundColor: 'rgb(240, 235, 220)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'turboGrafx16',
     name: 'TurboGrafx16',
     style: {
-      color: 'rgb(255, 163, 85)',
-      backgroundColor: 'rgb(48, 43, 74)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(245, 175, 100)',
+      backgroundColor: 'rgb(60, 55, 80)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'le',
     name: 'LE',
     style: {
-      color: 'rgb(66, 66, 69)',
-      backgroundColor: 'rgb(185, 205, 213)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(75, 75, 80)',
+      backgroundColor: 'rgb(175, 195, 205)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'ye',
     name: 'ye',
     style: {
-      color: 'rgb(33, 241, 81)',
-      backgroundColor: 'rgb(21, 44, 64)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(50, 230, 95)',
+      backgroundColor: 'rgb(30, 55, 75)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'ksg',
     name: 'KSG',
     style: {
-      color: 'rgb(255, 164, 31)',
-      backgroundColor: 'rgb(2, 27, 51)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(245, 175, 50)',
+      backgroundColor: 'rgb(15, 40, 65)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'gaj2018',
     name: 'GAJ (2018)',
     style: {
-      color: 'rgb(255, 255, 255)',
-      backgroundColor: 'rgb(189, 123, 194)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(245, 245, 245)',
+      backgroundColor: 'rgb(175, 135, 185)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'yandhi',
     name: 'Yandhi',
     style: {
-      color: 'rgb(166, 77, 121)',
-      backgroundColor: 'rgb(241, 235, 255)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(155, 90, 130)',
+      backgroundColor: 'rgb(235, 225, 245)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'jik',
     name: 'JIK',
     style: {
-      color: 'rgb(252, 220, 77)',
-      backgroundColor: 'rgb(24, 45, 156)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(240, 210, 90)',
+      backgroundColor: 'rgb(40, 60, 140)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'godsCountry',
     name: "God's Country",
     style: {
-      color: 'rgb(186, 204, 211)',
-      backgroundColor: 'rgb(30, 30, 108)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(175, 195, 200)',
+      backgroundColor: 'rgb(45, 45, 95)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'dondaV1',
     name: 'DONDA [V1]',
     style: {
-      color: 'rgb(135, 255, 93)',
-      backgroundColor: 'rgb(232, 65, 37)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(150, 245, 110)',
+      backgroundColor: 'rgb(210, 80, 55)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'dondaV2',
     name: 'Donda [V2]',
     style: {
-      color: 'rgb(235, 219, 183)',
-      backgroundColor: 'rgb(57, 138, 150)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(225, 210, 175)',
+      backgroundColor: 'rgb(70, 125, 135)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'dondaV3',
     name: 'Donda [V3]',
     style: {
-      color: 'rgb(255, 255, 255)',
-      backgroundColor: 'rgb(2, 3, 5)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(240, 240, 240)',
+      backgroundColor: 'rgb(15, 20, 25)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'donda2',
     name: 'Donda 2',
     style: {
-      color: 'rgb(48, 32, 34)',
-      backgroundColor: 'rgb(200, 201, 215)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(60, 45, 50)',
+      backgroundColor: 'rgb(185, 186, 200)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'war',
     name: 'WAR',
     style: {
-      color: 'rgb(213, 219, 232)',
-      backgroundColor: 'rgb(0, 0, 0)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(200, 205, 220)',
+      backgroundColor: 'rgb(15, 15, 15)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'yebu',
     name: 'YEBU',
     style: {
-      color: 'rgb(250, 59, 59)',
-      backgroundColor: 'rgb(41, 51, 65)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(235, 80, 80)',
+      backgroundColor: 'rgb(55, 65, 75)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'bbpb',
     name: 'BBPB',
     style: {
-      color: 'rgb(0, 0, 0)',
-      backgroundColor: 'rgb(171, 157, 122)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(25, 25, 25)',
+      backgroundColor: 'rgb(160, 145, 135)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   },
   {
     id: 'vultures1',
     name: 'VULTURES 1',
     style: {
-      color: 'rgb(185, 136, 93)',
-      backgroundColor: 'rgb(46, 40, 30)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(175, 145, 105)',
+      backgroundColor: 'rgb(60, 55, 45)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'vultures2',
     name: 'VULTURES 2',
     style: {
-      color: 'rgb(191, 182, 155)',
-      backgroundColor: 'rgb(31, 33, 31)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(180, 170, 145)',
+      backgroundColor: 'rgb(45, 48, 45)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'bully',
     name: 'BULLY',
     style: {
-      color: 'rgb(183, 183, 183)',
-      backgroundColor: 'rgb(67, 67, 67)',
-      borderColor: 'rgba(255, 255, 255, 0.52)'
+      color: 'rgb(170, 170, 170)',
+      backgroundColor: 'rgb(80, 80, 80)',
+      borderColor: 'rgba(255, 255, 255, 0.45)'
     }
   },
   {
     id: 'other',
     name: 'Other',
     style: {
-      color: 'rgb(0, 0, 0)',
-      backgroundColor: 'rgb(255, 255, 255)',
-      borderColor: 'rgba(0, 0, 0, 0.52)'
+      color: 'rgb(25, 25, 25)',
+      backgroundColor: 'rgb(245, 245, 245)',
+      borderColor: 'rgba(0, 0, 0, 0.45)'
     }
   }
 ];
