b91181597c1b15af532f175358e2fde261cd3c67
Mitch <mitch.hare03@gmail.com>
2025-07-08 15:24:03 +1000

    Remove leftover code from previous Home page implementation

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,23 +1,11 @@
-import React, { Suspense, useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { Helmet } from 'react-helmet';
 
-// JSX UI
-import { Fallback } from '/imports/ui/components/Fallback';
 import { CompShelf } from '../components/CompShelf';
 import { Meteor } from 'meteor/meteor';
 
 export const Home = () => {
-  const [topComps, setTopComps] = useState([
-    {
-      id: 'MISSING ID',
-      name: 'MISSING NAME',
-      yeditor: 'MISSING YEDITOR ID',
-      yeditor_name: 'MISSING YEDITOR NAME',
-      art_path: 'MISSING PATH'
-    }
-  ]);
-
-  // Create a function that captures current search context
+  // Create a function to pass to the Top 5 Comps shelf to give itself data
   const createLoadMoreFunc = useCallback(({ onSuccess, onError }) => {
     Meteor.call('getTop5Comps', {}, (err, result) => {
       if (err) onError(err);
@@ -33,11 +21,7 @@ export const Home = () => {
       <div className="flex justify-center w-full">
         <div>
           <h1 className="text-3xl font-bold mt-2">Welcome to Yedits.net!</h1>
-          {/* Suspense delays rendering until asynchronous data is ready (SSR) */}
-          <Suspense fallback={<Fallback />}>
-            <CompShelf items={topComps} onLoadMore={createLoadMoreFunc} />
-          </Suspense>
-          <Suspense fallback={<Fallback />}></Suspense>
+          <CompShelf onLoadMore={createLoadMoreFunc} />
         </div>
       </div>
     </>
