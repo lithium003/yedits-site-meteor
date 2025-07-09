3afaeeb44733f100747f7aec31d559f0ceda3f7f
Mitch <mitch.hare03@gmail.com>
2025-07-09 15:52:38 +1000

    Add section headers and larger centered title to Home page

--- imports/ui/pages/Home.jsx
+++ imports/ui/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
 
-import { useMeteorLoader } from '../../hooks/useMeteorLoader';
 import { COMPS, EDITS } from '../../api/collections/AvailableCollections';
+import { useMeteorLoader } from '../../hooks/useMeteorLoader';
 import { CompShelf } from '../components/CompShelf';
+import { SectionHeader } from '../components/SectionHeader';
 
 export const Home = () => {
   // Create a function to pass to the Top 5 Comps shelf to give itself data
@@ -16,8 +17,12 @@ export const Home = () => {
       </Helmet>
       <div className="flex justify-center w-full">
         <div>
-          <h1 className="text-3xl font-bold mt-2">Welcome to Yedits.net!</h1>
+          <h1 className="text-6xl font-bold my-2 text-center ">
+            Welcome to Yedits.net!
+          </h1>
+          <SectionHeader title="Top Comps" />
           <CompShelf onLoadMore={loadTopWorks} collection={COMPS} />
+          <SectionHeader title="Top Edits" />
           <CompShelf onLoadMore={loadTopWorks} collection={EDITS} />
         </div>
       </div>
