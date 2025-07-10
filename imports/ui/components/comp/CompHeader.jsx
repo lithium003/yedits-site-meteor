5307abe7b61df697bf0a9790ef439666c5f15dbf
Mitch <mitch.hare03@gmail.com>
2025-07-10 20:16:23 +1000

    Add link to yeditor page

--- imports/ui/components/comp/CompHeader.jsx
+++ imports/ui/components/comp/CompHeader.jsx
@@ -7,10 +7,9 @@ import {
 import React, { useState } from 'react';
 import { PillCheckbox } from '../search/PillCheckbox';
 import { getDateFromTimestamp } from '/imports/utils/firestoreHandling';
+import { Link } from 'react-router-dom';
 
 export const CompHeader = ({ comp }) => {
-  const [description, setDescription] = useState('a');
-
   return (
     <>
       <section className="text-center mb-8 w-full ">
@@ -28,9 +27,11 @@ export const CompHeader = ({ comp }) => {
             <h1 className="text-7xl font-black bg-gradient-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent leading-tight">
               {comp.name}
             </h1>
-            <p className="text-3xl font-bold text-indigo-300 mb-2">
-              {comp.yeditor_name || 'Unknown Yeditor'}
-            </p>
+            <Link to={`/yeditor/${comp.yeditor}`}>
+              <span className="text-3xl font-bold text-indigo-300 mb-2">
+                {comp.yeditor_name || 'Unknown Yeditor'}
+              </span>
+            </Link>
             <p className="text-xl text-gray-300 mb-2 max-w-2xl">
               {comp.desc || 'No description provided.'}
             </p>
@@ -77,9 +78,7 @@ export const CompHeader = ({ comp }) => {
               <PillCheckbox
                 label="Play All"
                 icon={faPlay}
-                onClick={() => {
-                  setDescription('An amazing comp with a lot of great edits');
-                }}
+                onClick={() => {}}
                 type="tag"
                 activeColor="bg-blue-500"
                 inactiveColor="bg-gray-300"
@@ -90,11 +89,7 @@ export const CompHeader = ({ comp }) => {
               <PillCheckbox
                 label="Download"
                 icon={faDownload}
-                onClick={() => {
-                  setDescription(
-                    'Community comp envisioning what GAJ would sound like if it were a B-side to MBDTF and as release ready as possible while still being creative'
-                  );
-                }}
+                onClick={() => {}}
                 type="tag"
                 activeColor="bg-blue-500"
                 inactiveColor="bg-gray-300"
@@ -105,11 +100,7 @@ export const CompHeader = ({ comp }) => {
               <PillCheckbox
                 label="Share"
                 icon={faShareAlt}
-                onClick={() => {
-                  setDescription(
-                    'LP6 comp containing various reworks and recreations of songs based on established and newly surfaced material from the era along with samples from the movie American Psycho inspired by BSB Dantes BAP comp.	'
-                  );
-                }}
+                onClick={() => {}}
                 type="tag"
                 activeColor="bg-blue-500"
                 inactiveColor="bg-gray-300"
