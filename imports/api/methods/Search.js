1bdbcf164eb1e8570714bb4c81ab1188f09924e0
Mitch <mitch.hare03@gmail.com>
2025-07-23 14:59:08 +1000

    Refactor Search.js methods

--- imports/api/methods/Search.js
+++ imports/api/methods/Search.js
@@ -6,7 +6,8 @@ import {
   EDITS,
   YEDITORS
 } from '/imports/api/collections/AvailableCollections';
-import { convertPath, db } from '/server/Firestore';
+import { db } from '/server/Firestore';
+import { getDocsWithConvertedPaths, handleMethod } from '/server/utils';
 
 Meteor.methods({
   async getSearchResults({
@@ -18,34 +19,27 @@ Meteor.methods({
     lastId = null
   }) {
     const numResults = 5;
-    try {
-      // Ordering by name_search before rating would be more performance- and cost-
-      // efficient, but this would mean when you search for "vultures" you get
-      // all comps called 'vultures' before ones called 'vultures 2' even if they're
-      // lowly rated random vultures comps.
-      let query = await db.collection(collection);
+    return handleMethod(async () => {
+      let query = db.collection(collection);
+
       if (era) {
         if (collection !== YEDITORS) {
           query = query.where('era', '==', era);
         } else {
-          // Avoid unnecessary query, as yeditors don't have eras and wont be displayed
           return {};
         }
       }
-      // TODO find a way to avoid this repeated yeditor special case check
-      // Yeditors don't have tags, so don't search for them.
-      // Since you're ALWAYS searching with tags unless you disable all of them, BUT WE NEVER DO THIS BC ARRAY_CONTAINS_ANY DOESNT WORK WITH AN EMPTY ARRAY
-      // we still want to show yeditors even when tags are specified. JUST HAVE TO REQUIRE AT LEAST ONE TAG. ALTERNATIVELY COULD USE A DUMMY TAG TO SYMBOLIZE EMPTY
+
       if (collection !== YEDITORS) {
         query = query.where('tags', 'array-contains-any', tags);
         if (artistId) {
-          if (artistId === '~') {
-            artistId = 'NULL';
-          }
           query = query.where('artist', '==', artistId);
         }
       }
-
+      // Ordering by name_search before rating would be more performance- and cost-
+      // efficient, but this would mean when you search for "vultures" you get
+      // all comps called 'vultures' before ones called 'vultures 2' even if they're
+      // lowly rated random vultures comps.
       switch (collection) {
         case COMPS:
           query = query
@@ -75,49 +69,25 @@ Meteor.methods({
             .limit(numResults);
           break;
         default:
-          console.error(`Error fetching results for ${collection}`);
-        // throw new Meteor.Error('firebase-error', 'Failed to fetch results');
+          throw new Meteor.Error(
+            'firebase-error',
+            `Unknown collection: ${collection}`
+          );
       }
 
-      // We need the actual document ID, but we modify its elements when we return them as a list.
-      // TODO: Find a way to keep the doc so we don't have to get() it again
       if (lastId) {
         const lastDoc = await db.collection(collection).doc(lastId).get();
         query = query.startAfter(lastDoc);
       }
 
-      const snapshot = await query.get();
-      // noinspection UnnecessaryLocalVariableJS
-      const results = snapshot.docs.map(doc => {
-        const data = doc.data();
-
-        if (data.art_path) {
-          data.art_path = convertPath(data.art_path);
-        }
-
-        if (data.filepath) {
-          data.filepath = convertPath(data.filepath);
-        }
-
-        return {
-          id: doc.id,
-          ...data
-        };
-      });
-
-      return results;
-    } catch (error) {
-      console.error(`Error fetching results for ${collection}: ${error}`);
-      throw new Meteor.Error('firebase-error', 'Failed to fetch results');
-    }
+      return await getDocsWithConvertedPaths(query);
+    }, `Error fetching results for ${collection}`);
   },
 
   async getAllArtists() {
-    try {
-      const query = db.collection(ARTISTS).get();
-      const snapshot = await query;
-      // noinspection UnnecessaryLocalVariableJS
-      const results = snapshot.docs
+    return handleMethod(async () => {
+      const snapshot = await db.collection(ARTISTS).get();
+      return snapshot.docs
         .map(doc => {
           const data = doc.data();
           return {
@@ -126,10 +96,6 @@ Meteor.methods({
           };
         })
         .filter(artist => artist.name);
-      return results;
-    } catch (error) {
-      console.error(`Error fetching list of all artists: ${error}`);
-      throw new Meteor.Error('firebase-error', 'Failed to fetch artists');
-    }
+    }, 'Error fetching list of all artists');
   }
 });
